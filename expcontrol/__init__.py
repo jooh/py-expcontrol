9ad2297d6e8f29b11b2bf3505a648a926fae75fa
Johan Carlin <johan.carlin@gmail.com>
2016-04-28 15:34:43 +0100

    appropriate super calls when subclassing to avoid bad self.starttime setting

--- expcontrol/__init__.py
+++ expcontrol/__init__.py
@@ -1,6 +1,6 @@
 # NB no automatic import of modules with external dependencies
 # (psychopydep, eyelinkdep)
 __all__ = ['base','event']
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 import base
 import event
