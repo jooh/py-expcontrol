19764e58ec857b5d7258623740470a30a8b58a54
Johan Carlin <johan.carlin@gmail.com>
2016-05-13 12:22:46 +0100

    v0.2.2

--- expcontrol/__init__.py
+++ expcontrol/__init__.py
@@ -1,6 +1,6 @@
 # NB no automatic import of modules with external dependencies
 # (psychopydep, eyelinkdep)
 __all__ = ['base','event']
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 import base
 import event
